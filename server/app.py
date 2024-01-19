57f8205872697c136500074e041b18ac2f3e0bfd
Elijah <ekollie324@gmail.com>
2024-01-19 18:43:45 -0500

    added server skeleton

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask_migrate import Migrate
+from flask import Flask, request, make_response
+from flask_restful import Api, Resource
+
+from config import app, db, api
+from models import *
+
+@app.route("/")
+def index():
+    return "<h1> Project Server </h1>"
+
+
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
